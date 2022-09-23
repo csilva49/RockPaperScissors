448bf9caee20942b7d6b558db7244d3dabc0a68b
silva <silva@pop-os.localdomain>
2022-09-23 22:16:39 +0200

    Score added

--- app.py
+++ app.py
@@ -1,44 +1,60 @@
 #random to enable the computer to pick an answer
 import random
 
-#initial variables
-user_hand = input('Enter your answer (a) Rock,(b) Paper,(c) Scissors) : ')
-computer_answer_list = ['Rock', 'Paper', 'Scissors']
-computer_hand = random.choice(computer_answer_list)
 
-def play_game(user_hand, computer_hand):
+#initial variables
+user_hand = ""
+computer_answer_list = ['Rock', 'Paper', 'Scissors']
+score = 0
+
+#check if user ended the game
+while user_hand != 'd':
+    user_hand = input('Enter your answer (a) Rock,(b) Paper,(c) Scissors,(d) End Game  : ')
+    computer_hand = random.choice(computer_answer_list)
     #test answer
     if user_hand == 'a' or user_hand == 'b' or user_hand == 'c':
         #if computer picks rock
         if computer_hand == 'Rock':
             if user_hand == 'a':
-                return print('The computer picked Rock, it\'s a draw!')
+                print('The computer picked Rock, it\'s a draw!\n\n Your score is  ' + str(score))
             elif user_hand == 'b':
-                return print('The computer picked Rock, you win!')
+                #if user wins, add 1 point to board
+                score += 1
+                print('The computer picked Rock, you win!\n\n Your score is  ' + str(score))
             else:
-                return print('The computer picked Rock, you lost!')
+                #if user loses, substract 1 point to board
+                score -= 1
+                print('The computer picked Rock, you lost!\n\n Your score is  ' + str(score))
 
         #if computer picks paper
         if computer_hand == 'Paper':
             if user_hand == 'a':
-                return print('The computer picked Paper, you lost!')
+                #if user loses, substract 1 point to board
+                score -= 1
+                print('The computer picked Paper, you lost!\n\n Your score is  ' + str(score))
             elif user_hand == 'b':
-                return print('The computer picked Paper, it\'s a draw!')
+                print('The computer picked Paper, it\'s a draw!\n\n Your score is  ' + str(score))
             else:
-                return print('The computer picked Paper, you win!')
-    
+                #if user wins, add 1 point to board
+                score += 1
+                print('The computer picked Paper, you win!\n\n Your score is  ' + str(score))
+
         #if computer picks scissors
         if computer_hand == 'Scissors':
             if user_hand == 'a':
-                return print('The computer picked Scissors, you win!')
+                #if user wins, add 1 point to board
+                score += 1
+                print('The computer picked Scissors, you win!\n\n Your score is  ' + str(score))
             elif user_hand == 'b':
-                return print('The computer picked Scissors, you lost!')
+                #if user loses, substract 1 point to board
+                score -= 1
+                print('The computer picked Scissors, you lost!\n\n Your score is  ' + str(score))
             else:
-                return print('The computer picked Scissors, it\'s a draw!')
+                print('The computer picked Scissors, it\'s a draw!\n\n Your score is  ' + str(score))
     else:
         print('You choose an impossible hand, please restart the game...')
+else:
+    print('You have ended the game! Your final score is '+ str(score))
     
 
-#start the game
-play_game(user_hand, computer_hand)
 
