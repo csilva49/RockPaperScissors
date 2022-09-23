d198a1304bff407225ac146d6bb43e6ae63482e9
silva <silva@pop-os.localdomain>
2022-09-23 21:29:59 +0200

    Initial Configuration (Variables)

---
+++ app.py
@@ -0,0 +1,12 @@
+from re import S
+
+
+user_answer = input('Enter your answer (Rock, Paper, Scissors')
+computer_answer = ['Rock', 'Paper', 'Scissors']
+
+def play_game(user_answer):
+    print(user_answer)
+
+
+play_game(user_answer)
+
