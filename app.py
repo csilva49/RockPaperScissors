f84a82b79921de10445f6451f4f282bd3d55274d
silva <silva@pop-os.localdomain>
2022-09-23 21:35:48 +0200

    Added computers hand

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from re import S
+#random to enable the computer to pick an answer
+import random
 
 
-user_answer = input('Enter your answer (Rock, Paper, Scissors')
-computer_answer = ['Rock', 'Paper', 'Scissors']
+user_hand = input('Enter your answer (Rock, Paper, Scissors)')
+computer_answer_list = ['Rock', 'Paper', 'Scissors']
+computer_hand = random.choice(computer_answer_list)
 
-def play_game(user_answer):
-    print(user_answer)
+def play_game(user_hand, computer_hand):
+    print(computer_hand)
+    print(user_hand)
 
 
-play_game(user_answer)
+play_game(user_hand, computer_hand)
 
