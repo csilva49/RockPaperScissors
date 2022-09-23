bcaba0559626460915e418ff56e9b6a136301f33
silva <silva@pop-os.localdomain>
2022-09-23 21:50:55 +0200

    Finished code, it examines the users answer and only allows itself to play when the right answer is given either a b or c

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
 #random to enable the computer to pick an answer
 import random
 
-
-user_hand = input('Enter your answer (Rock, Paper, Scissors)')
+#initial variables
+user_hand = input('Enter your answer (a) Rock,(b) Paper,(c) Scissors) : ')
 computer_answer_list = ['Rock', 'Paper', 'Scissors']
 computer_hand = random.choice(computer_answer_list)
 
 def play_game(user_hand, computer_hand):
-    print(computer_hand)
-    print(user_hand)
+    #test answer
+    if user_hand == 'a' or user_hand == 'b' or user_hand == 'c':
+        #if computer picks rock
+        if computer_hand == 'Rock':
+            if user_hand == 'a':
+                return print('The computer picked Rock, it\'s a draw!')
+            elif user_hand == 'b':
+                return print('The computer picked Rock, you win!')
+            else:
+                return print('The computer picked Rock, you lost!')
 
+        #if computer picks paper
+        if computer_hand == 'Paper':
+            if user_hand == 'a':
+                return print('The computer picked Paper, you lost!')
+            elif user_hand == 'b':
+                return print('The computer picked Paper, it\'s a draw!')
+            else:
+                return print('The computer picked Paper, you win!')
+    
+        #if computer picks scissors
+        if computer_hand == 'Scissors':
+            if user_hand == 'a':
+                return print('The computer picked Scissors, you win!')
+            elif user_hand == 'b':
+                return print('The computer picked Scissors, you lost!')
+            else:
+                return print('The computer picked Scissors, it\'s a draw!')
+    else:
+        print('You choose an impossible hand, please restart the game...')
+    
 
+#start the game
 play_game(user_hand, computer_hand)
 
